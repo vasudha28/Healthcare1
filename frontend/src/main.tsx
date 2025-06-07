46fe9ec09b64b0471d838bec553e07a544bbefc9
Vasudha28 <donthamsettivasudha@gmail.com>
2025-06-07 10:40:39 +0530

    css fixes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './styles/globals.css'
 
-const rootElement = document.getElementById('root')
-if (!rootElement) throw new Error('Failed to find the root element')
-
-const root = createRoot(rootElement)
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 )
